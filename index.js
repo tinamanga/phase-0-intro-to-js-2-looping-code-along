683c7086f5949496c4447bf5f24b16181c642137
tinamanga <christinamanga28@gmail.com>
2025-04-05 00:30:58 +0300

    using for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+//writeCards() function
+function writeCards(names, event) {
+    const messages = []; // This will store the thank-you messages
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message); // Add the message to the array
+    }
+    return messages; // Return the array of messages
+  }
+  
+// countDown() function using while loop
+  function countDown(start) {
+    let count = start;
+    while (count >= 0) {
+      console.log(count);
+      count--; // Decrement count to move towards 0
+    }
+  }
+  
+  
